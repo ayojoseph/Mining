5bfdb0bfac0090c720bc8ffb077a41bf5e213a14
Unknown <ljoseph@mail.uoguelph.ca>
2018-11-15 11:22:24 -0500

    links work

--- scrape.js
+++ scrape.js
@@ -6,40 +6,68 @@ const fs = require("fs");
 var fstream = fs.readFileSync("output.txt").toString();
 var names = fstream.split("\n");
 
+
+//used to generate weblinks
+
+// var lsFile = fs.createWriteStream('links.txt', {
+//   flags: 'a' // 'a' means appending (old data will be preserved)
+// });
+
+// for (var i = 0; i < names.length; i++) {
+  
+//               lsFile.write("https://www.cnarea.ca/web-2-0-directory/"+names[i]+"\n");
+// }
+
+
+
 // console.log(names[584]);
 
-var coun;
+// var count;
+// for (var looper = 0; looper < 2; looper++) {
+//   for (count = 10 *looper; count < 10+(looper*10); count++) {
+//     request(
+//       "https://www.cnarea.ca/web-2-0-directory/" + names[count],
+//       (error, response, html) => {
+//         if (!error && response.statusCode == 200) {
+//           const $ = cheerio.load(html);
+//           //   const heading = $(".member-profile-page-title");
+//           const aName = $(
+//             ".w2dc-listing-header"
+//           );
+//           // console.log(aName.text());
 
-for (count = 0; count < 1; count++) {
-  request(
-    "https://www.cnarea.ca/web-2-0-directory/" + names[count],
-    (error, response, html) => {
-      if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-        //   const heading = $(".member-profile-page-title");
-        const heading = $(
-          ".w2dc-field w2dc-field-output-block w2dc-field-output-block-address w2dc-field-output-block-2"
-        );
-        // console.log(heading.html());
-        console.log(heading.text());
-        // console.log(html);
+//           const aLocation = $(
+//             ".w2dc-location"
+//           );
+//           // console.log(aLocation.text());
 
-        //outputting html to file test
-        // fs.writeFile("outter.html", html, err => {
-        //   if (err) throw err;
-        // });
-      }
-    }
-  );
-}
+//           const aContact = $(
+//             ".w2dc-fields-group"
+//           );
 
-// request("https://www.cnarea.ca/find-an-appraiser/", (error, response, html) => {
-//   if (!error && response.statusCode == 200) {
-//     const $ = cheerio.load(html);
-//     //   const heading = $(".member-profile-page-title");
-//     const heading = $(".w2dc-listings-block-content");
-//     console.log(heading.html());
-//     // console.log(heading.text());
-//     // console.log(html);
+//           var contact = aContact.text().replace(/\s+/g, '');; 
+//           // console.log(aContact.text());
+//           var logger = fs.createWriteStream('log.txt', {
+//             flags: 'a' // 'a' means appending (old data will be preserved)
+//           });
+//           logger.write(contact+"\n");
+
+//           // console.log(heading.html());
+//           //
+          
+//           // console.log(html);
+
+//           // outputting html to file test
+//           // fs.writeFile("outter.html", html, err => {
+//           //   if (err) throw err;
+//           // });
+//         } 
+//         // else {
+//         //   console.log("Error! Count is: "+count);
+//         //   count = 99;
+//         // }
+//       }
+//     );
 //   }
-// });
+// }
+
